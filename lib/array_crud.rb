7bf73595b8d46192fdd62cd61a80311d22d6665c
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-03 21:25:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["dog", "cat", "fish", "bird"]
-  array .unshift("#{element}")
+  bird
 end
 
 def remove_element_from_start_of_array(array)
